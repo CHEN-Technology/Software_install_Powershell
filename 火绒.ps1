7c6f4b60e579dcf47717b254c9b246d2cc63f3e2
CHEN-Technology <chenkai051123@outlook.com>
2024-05-24 08:53:11 +0800

    创建 火绒.ps1

---
+++ 火绒.ps1
@@ -0,0 +1,34 @@
+$URL = "https://down-tencent.huorong.cn/sysdiag-all-x64-6.0.0.29-2024.05.22.1.exe"
+$OUTPUT_FILE = "火绒.exe"
+
+# 检查文件是否存在
+if (!(Test-Path $OUTPUT_FILE)) {
+    Write-Host "文件不存在，开始下载"
+    # 下载文件
+    $webRequest = Invoke-WebRequest -Uri $URL -OutFile $OUTPUT_FILE
+    Write-Host "下载完成，运行可执行文件"
+    # 启动可执行文件
+    Start-Process $OUTPUT_FILE
+} else {
+    Write-Host "文件存在，开始检查完整性"
+    # 获取远端文件的大小
+    $remoteSize = (Invoke-WebRequest -Uri $URL -Method Head).Headers['Content-Length']
+
+    # 获取本地文件的大小
+    $localSize = (Get-Item $OUTPUT_FILE).Length
+
+    if ($remoteSize -eq $localSize) {
+        Write-Host "文件存在且完整"
+        # 启动可执行文件
+        Start-Process $OUTPUT_FILE
+    } else {
+        Write-Host "文件不存在或损坏，重新下载"
+        # 删除现有文件
+        Remove-Item $OUTPUT_FILE
+        # 重新下载文件
+        Invoke-WebRequest -Uri $URL -OutFile $OUTPUT_FILE
+        Write-Host "下载完成，运行可执行文件"
+        # 启动可执行文件
+        Start-Process $OUTPUT_FILE
+    }
+}
