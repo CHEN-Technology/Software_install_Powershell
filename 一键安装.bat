bdf093654bb8883f724ebf94d69ff95e0a18a012
CHEN-Technology <chenkai051123@outlook.com>
2024-05-24 08:53:08 +0800

    创建 一键安装.bat

---
+++ 一键安装.bat
@@ -0,0 +1,7 @@
+@echo off
+powershell -ExecutionPolicy RemoteSigned -File .\¶¤¶¤.ps1
+powershell -ExecutionPolicy RemoteSigned -File .\»ðÈÞ.ps1
+powershell -ExecutionPolicy RemoteSigned -File .\Î¢ÐÅ.ps1
+powershell -ExecutionPolicy RemoteSigned -File .\QQ.ps1
+powershell -ExecutionPolicy RemoteSigned -File .\Bandizip.ps1
+powershell -ExecutionPolicy RemoteSigned -File .\PotPlayer.ps1
